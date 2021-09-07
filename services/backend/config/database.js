f338d3d8dbb78d29f9cef58d82d867ce313a1722
Maxim Shuckin <shuckin@mail.ru>
2021-09-07 21:53:46 +0300

    Update database.js

--- services/backend/config/database.js
+++ services/backend/config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'romenderos.9hbq9.mongodb.net'),
+        host: env('DATABASE_HOST', ''),
         srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'VeyroN910'),
+        port: env.int('DATABASE_PORT', ),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
